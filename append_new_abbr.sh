e3f43f2177e73c092036745ba29f027c0009d205
Vitalizzare <vitalizzare@gmail.com>
2021-05-16 19:36:08 +0300

    update append_new: delete commas, keep expl case, mask /\ chars

--- append_new_abbr.sh
+++ append_new_abbr.sh
@@ -1,26 +1,42 @@
 #!/usr/bin/env bash
+
 abbr_file="${1:-abbr.csv}"
-echo 'Avoid comma and slash signs'
-echo 'Press Ctrl+D to stop'
+
+duplicates="$(./check_abbr_duplicates.sh "$abbr_file")"
+[ "x$duplicates" = "x" ] || \
+{
+    echo -e "Resolve duplicate before continue:\n$duplicates" 
+    exit 1
+}
+
+echo 'Press Ctrl+C to stop'
+
 while read -e -p 'ABBR: ' abbr
 do
+    if ! expr "$abbr" : "^[[:alnum:]]\+\$">/dev/null; then continue; fi
+    # abbr to upper case
+    abbr="${abbr^^}"
     read -e -p 'EXPLANATION: ' explanation
-    previous="$(sed -n -e "/^${abbr^^},/p" "$abbr_file")"
+    # delete any possible comma in explanation
+    explanation="${explanation//,}"
+    previous="$(grep "^$abbr," "$abbr_file")"
     if [ "x$previous" = "x" ]
     then
-        echo "${abbr^^},${explanation,,}" >> "$abbr_file"
+        echo "${abbr},${explanation}" >> "$abbr_file"
     else
         echo "Previous entry found:"
         echo "OLD: ${previous%%,*} = ${previous#*,}"
-        echo "NEW: ${abbr^^} = ${explanation,,}"
+        echo "NEW: ${abbr} = ${explanation}"
         read -e -p "Update? [Y/n]: " upd
         case "$upd" in
         n|N|no|No|NO)
             echo "Saved old: ${previous%%,*} = ${previous#*,}"
             ;;
         *)
-            sed -i -e "s/^${abbr^^},.*$/${abbr^^},${explanation,,}/" "$abbr_file"
-            echo "Saved new: ${abbr^^} = ${explanation,,}"
+            echo "Saved new: ${abbr} = ${explanation}"
+            # mask some chars with a backslash sign
+            explanation="$(echo $explanation | sed -e 's/[/\]/\\&/g')"
+            sed -i -e "s/^${abbr},.*$/${abbr},${explanation}/" "$abbr_file"
         esac
     fi
 done
