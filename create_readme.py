fa40a1e566929cfc59f1a6fd4edaf2118f8f6588
Vitalizzare <vitalizzare@gmail.com>
2021-05-16 11:44:49 +0300

    update create_readme.py - CLI args $1=[csv to read], $2=[file to write]

--- create_readme.py
+++ create_readme.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
 import pandas as pd
+import sys
 
+abbr_file = 'abbr.csv' if len(sys.argv) == 1 else sys.argv[1]
+readme_file = 'README.md' if len(sys.argv) <= 2 else sys.argv[2]
 
-abbr_file = 'abbr.csv'
-readme_file = 'README.md'
-data = pd.read_csv(abbr_file, index_col=0, header=None, names=['ABBR'], squeeze=True)
+data = pd.read_csv(abbr_file, index_col=0, header=None, squeeze=True)
 
 data = data.sort_index()
 with open(readme_file, 'w') as f:
-    f.write('ABBR|DESCRIPTION\n')
-    f.write(':---|:---\n')
+    f.write('ABBR|DESCRIPTION\n'
+            ':---|:---\n')
     for i in data.index:
         f.write(f'{i}|{data[i]}\n')
 
