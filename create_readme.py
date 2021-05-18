518e871266583cd6d7a319e63ac1a1e65b60b6f0
Vitalizzare <vitalizzare@gmail.com>
2021-05-18 13:03:59 +0300

    add try-except with sys.exit(...) to create_readme.py

--- create_readme.py
+++ create_readme.py
@@ -3,15 +3,20 @@
 import pandas as pd
 import sys
 
-abbr_file = 'abbr.csv' if len(sys.argv) == 1 else sys.argv[1]
-readme_file = 'README.md' if len(sys.argv) <= 2 else sys.argv[2]
-
-data = pd.read_csv(abbr_file, index_col=0, header=None, squeeze=True)
-
-data = data.sort_index()
-with open(readme_file, 'w') as f:
-    f.write('ABBR|DESCRIPTION\n'
-            ':---|:---\n')
-    for i in data.index:
-        f.write(f'{i}|{data[i]}\n')
-
+try:
+    abbr_file = 'abbr.csv' if len(sys.argv) == 1 else sys.argv[1]
+    readme_file = 'README.md' if len(sys.argv) <= 2 else sys.argv[2]
+    
+    data = pd.read_csv(abbr_file, index_col=0, header=None, squeeze=True)
+    
+    data = data.sort_index()
+    with open(readme_file, 'w') as f:
+        f.write('ABBR|DESCRIPTION\n'
+                ':---|:---\n')
+        for i in data.index:
+            f.write(f'{i}|{data[i]}\n')
+except Exception as e:
+    print(repr(e), file=sys.stderr)
+    sys.exit(1)
+else:
+    sys.exit(0)
